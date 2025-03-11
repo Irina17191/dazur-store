b999b7801fc8708e64b0c41f621159ae55b2f3e1
Iryna Honcharenko <irynahoncharenko9@gmail.com>
2025-03-11 22:39:27 +0200

    Create template for index page

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from catalog.views import hello_world
-
+from catalog.views import hello_world, index
 
 urlpatterns = [
     path("hello-world/", hello_world, name="hello_world"),
+    path("", index, name="index"),
 
 ]
 
